8d283125f01c4292098a0625b67c5caca76b89a8
phani kumar pilla <chiruaditya208@gmail.com>
2021-07-01 12:19:54 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const App=()=> {
   return (
     <Router>
       <Route path="/group-chat-app/" exact component={Join} />
-      <Route path="/Chat" component={Chat} />
+      <Route path="/Chat" exact component={Chat} />
     </Router>
   );
 }
